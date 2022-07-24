690eef88672f084955e454ef7e8747404ddb04bf
Ataniyaz_Kuanyshev <kuanyshev.ataniyaz@gmail.com>
2022-07-24 20:27:36 +0300

    testing

--- trn_db_tests.py
+++ trn_db_tests.py
@@ -4,65 +4,65 @@ from contextlib import contextmanager
 from variables import server, database, username, password
 
 
-# class MsDb:
-#     """ Collection of helper methods to query the MS SQL Server database.
-#     """
-#
-#     def __init__(self, username, password, server, initial_db='dev_db'):
-#         self.username = username
-#         self._password = password
-#         self.server = server
-#         self.db = initial_db
-#         conn_str = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server +\
-#                    ';DATABASE=' + self.db + \
-#                    ';UID=' + self.username + \
-#                    ';PWD=' + self._password + ';'
-#         print('Connected to DB:', conn_str)
-#         self._connection = pyodbc.connect(conn_str)
-#         pyodbc.pooling = False
-#
-#     def __repr__(self):
-#         return f"MS-SQLServer('{self.username}', <password hidden>, '{self.server}', '{self.db}')"
-#
-#     def __str__(self):
-#         return f"MS-SQLServer Module for STP on {self.server}"
-#
-#     def __del__(self):
-#         self._connection.close()
-#         print("Connection closed.")
-#
-#     @contextmanager
-#     def cursor(self, commit: bool = False):
-#         """
-#         A context manager style of using a DB cursor for database operations.
-#         This function should be used for any database queries or operations that
-#         need to be done.
-#
-#         :param commit:
-#         A boolean value that says whether to commit any database changes to the database. Defaults to False.
-#         :type commit: bool
-#         """
-#         cursor = self._connection.cursor()
-#         try:
-#             yield cursor
-#         except pyodbc.DatabaseError as err:
-#             print("DatabaseError {} ".format(err))
-#             cursor.rollback()
-#             raise err
-#         else:
-#             if commit:
-#                 cursor.commit()
-#         finally:
-#             cursor.close()
-#
-#
-# ms_db = MsDb(username=username, password=password, server=server, initial_db=database)
+class MsDb:
+    """ Collection of helper methods to query the MS SQL Server database.
+    """
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + 'host.docker.internal,54866' + ';DATABASE=' + 'TRN' + ';UID=' + 'test_user' + ';PWD=' + '1234' + ';')
-cursor = conn.cursor()
-cursor.execute('select count(employee_id) from hr.employees')
-output = cursor.fetchall()
-print(output)
+    def __init__(self, username, password, server, initial_db='dev_db'):
+        self.username = username
+        self._password = password
+        self.server = server
+        self.db = initial_db
+        conn_str = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server +\
+                   ';DATABASE=' + self.db + \
+                   ';UID=' + self.username + \
+                   ';PWD=' + self._password + ';'
+        print('Connected to DB:', conn_str)
+        self._connection = pyodbc.connect(conn_str)
+        pyodbc.pooling = False
+
+    def __repr__(self):
+        return f"MS-SQLServer('{self.username}', <password hidden>, '{self.server}', '{self.db}')"
+
+    def __str__(self):
+        return f"MS-SQLServer Module for STP on {self.server}"
+
+    def __del__(self):
+        self._connection.close()
+        print("Connection closed.")
+
+    @contextmanager
+    def cursor(self, commit: bool = False):
+        """
+        A context manager style of using a DB cursor for database operations.
+        This function should be used for any database queries or operations that
+        need to be done.
+
+        :param commit:
+        A boolean value that says whether to commit any database changes to the database. Defaults to False.
+        :type commit: bool
+        """
+        cursor = self._connection.cursor()
+        try:
+            yield cursor
+        except pyodbc.DatabaseError as err:
+            print("DatabaseError {} ".format(err))
+            cursor.rollback()
+            raise err
+        else:
+            if commit:
+                cursor.commit()
+        finally:
+            cursor.close()
+
+
+ms_db = MsDb(username=username, password=password, server=server, initial_db=database)
+
+# conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + 'host.docker.internal,54866' + ';DATABASE=' + 'TRN' + ';UID=' + 'test_user' + ';PWD=' + '1234' + ';')
+# cursor = conn.cursor()
+# cursor.execute('select count(employee_id) from hr.employees')
+# output = cursor.fetchall()
+# print(output)
 class TestTrnDB:
     @pytest.mark.jobs_table_tests
     def test_jobs_average_salary(self):
--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-server = 'localhost,54866'
+server = 'host.docker.internal,54866'
 database = 'TRN'
 username = 'test_user'
 password = '1234'
